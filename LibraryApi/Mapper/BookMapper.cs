9cd1bc9c73ed118b356134b33333063163d1378a
happy-skunk <makats.alexandre@gmail.com>
2025-05-03 13:18:08 +0400

    .

---
+++ LibraryApi/Mapper/BookMapper.cs
@@ -0,0 +1,43 @@
+﻿using LibraryApi.DTOs.Book;
+using LibraryApi.Models;
+
+namespace LibraryApi.Mapper
+{
+    public static class BookMapper
+    {
+        public static Book ToBook(this BookCreateDto dto) 
+        {
+            return new Book
+            {
+                Title = dto.Title,
+                AuthorId = dto.AuthorId,
+                GenreId = dto.GenreId,
+                Price = dto.Price,
+            };
+        }
+
+        public static Book ToBook(this BookUpdateDto dtp)
+        {
+            return new Book
+            {
+                Id = dtp.Id,
+                Title = dtp.Title,
+                AuthorId = dtp.AuthorId,
+                GenreId = dtp.GenreId,
+                Price = dtp.Price,
+            };
+        }
+
+        public static BookViewDto ToBookViewDto(this Book book) 
+        {
+            return new BookViewDto
+            {
+                Id = book.Id,
+                Title = book.Title,
+                Price = book.Price,
+                AuthorName = book.Author?.Name,
+                GenreName = book.Genre?.Name,
+            };
+        }
+    }
+}
--- LibraryApi/Services/BookService.cs
+++ LibraryApi/Services/BookService.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryApi.DTOs.Book;
+using LibraryApi.Mapper;
 using LibraryApi.Models;
 using LibraryApi.Repository.Specific;
 using Microsoft.EntityFrameworkCore;
@@ -17,13 +18,7 @@ namespace LibraryApi.Services
 
         public async Task<int> CreateBookAsync(BookCreateDto bookCreateDto)
         {
-            var book = new Book
-            {
-                Title = bookCreateDto.Title,
-                Price = bookCreateDto.Price,
-                AuthorId = bookCreateDto.AuthorId,
-                GenreId = bookCreateDto.GenreId,
-            };
+            var book = bookCreateDto.ToBook();
 
             await _bookRepo.Add(book);
             return book.Id;
@@ -31,14 +26,7 @@ namespace LibraryApi.Services
         public async Task<IEnumerable<BookViewDto>> GetAllBooksAsync()
         {
             var books = await _bookRepo.GetAllIncludingAsync();
-            return books.Select(book => new BookViewDto
-            {
-                Id = book.Id,
-                Title = book.Title,
-                Price = book.Price,
-                AuthorName = book.Author?.Name,
-                GenreName = book.Genre?.Name
-            });
+            return books.Select(BookMapper.ToBookViewDto);
         }
 
         public async Task<BookViewDto> GetBookByIdAsync(int id)
@@ -46,14 +34,7 @@ namespace LibraryApi.Services
             var book = await _bookRepo.GetByIdIncludingAsync(id);
             if (book == null) return null;
 
-            return new BookViewDto
-            {
-                Id = book.Id,
-                Title = book.Title,
-                Price = book.Price,
-                AuthorName = book.Author?.Name,
-                GenreName = book.Genre?.Name
-            };
+            return book.ToBookViewDto();
         }
 
         public async Task UpdateBookAsync(BookUpdateDto dto)
@@ -61,10 +42,7 @@ namespace LibraryApi.Services
             var book = await _bookRepo.GetById(dto.Id);
             if (book == null) return;
 
-            book.Title = dto.Title;
-            book.Price = dto.Price;
-            book.AuthorId = dto.AuthorId;
-            book.GenreId = dto.GenreId;
+            book = dto.ToBook();
 
             await _bookRepo.Update(book);
         }
@@ -89,36 +67,21 @@ namespace LibraryApi.Services
             var books = await _bookRepo.GetBooksByAuthorNameAsync(authorName);
             if (books == null) return null;
 
-            return books.Select(book => new BookViewDto
-            {
-                Id = book.Id,
-                Title = book.Title,
-                AuthorName = book.Author?.Name,
-            });
+            return books.Select(BookMapper.ToBookViewDto);
         }
         public async Task<IEnumerable<BookViewDto>> GetBooksByGenreNameAsync(string genreName)
         {
             var books = await _bookRepo.GetBooksByGenreNameAsync(genreName);
             if (books == null) return null;
 
-            return books.Select(book => new BookViewDto
-            {
-                Id = book.Id,
-                Title = book.Title,
-                GenreName = book.Genre.Name,
-            });
+            return books.Select(BookMapper.ToBookViewDto);
         }
         public async Task<IEnumerable<BookViewDto>> GetBooksByPriceRangeAsync(decimal Min, decimal Max)
         {
             var books = await _bookRepo.GetBooksByPriceRangeAsync(Min, Max);
             if (books == null) return null;
 
-            return books.Select(book => new BookViewDto
-            {
-                Id = book.Id,
-                Title = book.Title,
-                Price = book.Price,
-            });
+            return books.Select(BookMapper.ToBookViewDto);
         }
     }
 }
